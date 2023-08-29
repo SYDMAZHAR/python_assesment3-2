b4930579ac09ad7d0b91cd37e884d14b61fb4fb4
SYDMAZHAR <142782345+SYDMAZHAR@users.noreply.github.com>
2023-08-29 14:01:14 +0530

    Update python_assessment3.py

--- python_assessment3.py
+++ python_assessment3.py
@@ -1,18 +1,3 @@
-def sum_of_numbers(num_list):
-    total = sum(num_list)
-    return total
-
-# Sample List
-sample_list = [8, 2, 3, 0, 7]
-
-# Calculating the sum
-result = sum_of_numbers(sample_list)
-
-# Printing the result
-print("Expected Output:", result)
-
-
-
 def reverse_string(input_str):
     return input_str[::-1]
 
